adedf89842dd44df22340663835f1e1b5daa0cd4
zachnewburgh <zach.newburgh@gmail.com>
2017-07-08 14:15:25 -0400

    feat(input): Add label and attributes to input

--- src/app/field/input/input.component.ts
+++ src/app/field/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,8 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
+  @Input() type: string;
+  @Input() name: string;
+  @Input() label: string;
+  @Input() placeholder: string;
+  @Input() value: any;
+  @Input() disabled: boolean;
+  @Input() max: number;
+  @Input() min: number;
+  @Input() pattern: string;
+  @Input() readOnly: boolean;
+  @Input() required: boolean;
+  @Input() step: number;
 
-  constructor() { }
+  constructor() {
+    this.type = this.type || 'text';
+    this.label = this.label || 'label';
+    this.name = this.name || 'name';
+    this.placeholder = this.placeholder || 'placeholder';
+    this.value = this.value || 'value';
+  }
 
   ngOnInit() {
   }
