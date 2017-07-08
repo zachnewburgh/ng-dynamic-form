89ba823343b975ca81e4850961af5c5f460f83a9
zachnewburgh <zach.newburgh@gmail.com>
2017-07-08 14:01:15 -0400

    feat(textarea): Add textarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FieldComponent } from './field/field.component';
 import { InputComponent } from './field/input/input.component';
+import { TextareaComponent } from './field/textarea/textarea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FieldComponent,
-    InputComponent
+    InputComponent,
+    TextareaComponent
   ],
   imports: [
     BrowserModule
