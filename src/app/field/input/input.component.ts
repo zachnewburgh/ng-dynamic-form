b01f40c184227ddddd5bb5a7b37eceb588b82c5f
zachnewburgh <zach.newburgh@gmail.com>
2017-07-09 10:54:13 -0400

    refactor(input): Pass options into input

--- src/app/field/input/input.component.ts
+++ src/app/field/input/input.component.ts
@@ -6,28 +6,38 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
-  @Input() type: string;
-  @Input() name: string;
-  @Input() label: string;
-  @Input() placeholder: string;
-  @Input() value: any;
-  @Input() disabled: boolean;
-  @Input() max: number;
-  @Input() min: number;
-  @Input() pattern: string;
-  @Input() readOnly: boolean;
-  @Input() required: boolean;
-  @Input() step: number;
+  @Input() options: any;
+  disabled: boolean;
+  elementClass: string;
+  label: string;
+  max: number;
+  min: number;
+  name: string;
+  pattern: string;
+  placeholder: string;
+  readOnly: boolean;
+  required: boolean;
+  step: number;
+  type: string;
+  value: string;
 
   constructor() {
-    this.type = this.type || 'text';
-    this.label = this.label || 'label';
-    this.name = this.name || 'name';
-    this.placeholder = this.placeholder || 'placeholder';
-    this.value = this.value || 'value';
   }
 
   ngOnInit() {
+    this.disabled = this.options['disabled'];
+    this.elementClass = this.options['elementClass'];
+    this.label = this.options['label'];
+    this.max = this.options['max'];
+    this.min = this.options['mim'];
+    this.name = this.options['name'];
+    this.pattern = this.options['pattern'];
+    this.placeholder = this.options['placeholder'] || '';
+    this.readOnly = this.options['readOnly'];
+    this.required = this.options['required'];
+    this.step = this.options['step'];
+    this.type = this.options['type'];
+    this.value = this.options['value'] || '';
   }
 
 }
